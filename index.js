9c769797ab6505d218196c54a6ba729f1c8bbaf0
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-08-07 16:17:33 +0530

    food app

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".full-width-carousel").slick({
+    autoplay: true,
+    autoplaySpeed: 2000, // Change slide every 2 seconds
+    arrows: false,
+    dots: true,
+    pauseOnHover: false,
+  });
+});
---
+++ signin.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("signin-form");
+  const errorMessage = document.getElementById("error-message");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const username = form.querySelector("#username").value;
+    const password = form.querySelector("#password").value;
+
+    // Simulate a simple authentication check (replace with your actual authentication logic)
+    if (username === "user" && password === "password") {
+      // Redirect to home page
+      window.location.href = "home.html";
+    } else {
+      errorMessage.textContent = "Invalid username or password.";
+    }
+  });
+});
+
+// 
